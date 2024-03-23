051be2a6d833f9faad378b10285faaa8ef28331c
Macrodose <122926023+Macrodose@users.noreply.github.com>
2024-03-23 14:38:00 +0000

    basic functionality established

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,58 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+
+	// HTML Eles
+	const [task, setTask] = useState("");
+	const [toDoList, setToDoList] = useState([]);
+	const handleKeyDown = (event) => {
+		if (event.key === 'Enter') {
+			if(task === "") {
+				alert("Please enter something.");
+			} else if (toDoList.includes((task))) {
+				alert("Task already exists!")
+			} else {
+				console.log(task)
+				addTask(task)
+				setTask("");
+			}
+		};}
+
+		const addTask = (newTask) => {
+			setToDoList([...toDoList, newTask]);
+		}
+
+		const deleteTask = (taskToDelete) => {
+			setToDoList(prevToDoList => prevToDoList.filter(item => item !== taskToDelete));
+		}
+
+	const handleInputChange = (event) => {
+		setTask(event.target.value);
+	};
+
+	const listEmpty = () => toDoList.length === 0;
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex flex-column justify-content-center align-items-center">
+			<h1>My To Do List!</h1>
+			<input 
+			type="text"
+			value={task}
+			onChange={handleInputChange}
+			onKeyDown={handleKeyDown}
+			/>
+			{listEmpty() && <p>No tasks, add a task</p>}
+			<ul>
+				{toDoList.map((element, index) => (
+					<li key={index}>{element}
+					<i className="fas fa-trash" 
+					onClick={() => deleteTask(element)}
+					style={{ cursor: "pointer"}}
+					></i>
+					</li>
+					))}
+			</ul>
 		</div>
 	);
 };
